48d3709eb73e3da06d6826fc96ef3e5710e72498
Ankit kumar <125955449+Ankitkumar1100@users.noreply.github.com>
2023-07-15 09:41:41 +0530

    Update logical.c

--- logical.c
+++ logical.c
@@ -1,24 +1,25 @@
-#include<stdio.h>
+#include<stdio.h>  // This line includes the standard input/output library in the code.
+
 int main(){
-    int x=5,y=5,z=10,result;
+    int x=5, y=5, z=10, result;  // Declare and initialize integer variables x, y, z, and result.
 
-    result=(x==y)&&(z>y);
-    printf("(x==y)&& (z>y)is %d \n",result);
+    result=(x==y)&&(z>y);  // Assign the result of the logical expression (x==y)&&(z>y) to the variable result.
+    printf("(x==y)&& (z>y) is %d \n",result);  // Print the value of the logical expression (x==y)&&(z>y).
 
-    result= (x==y)&&(z<y);
-    printf("(x==y)&&(z<y)is %d\n",result);
+    result= (x==y)&&(z<y);  // Assign the result of the logical expression (x==y)&&(z<y) to the variable result.
+    printf("(x==y)&&(z<y) is %d\n",result);  // Print the value of the logical expression (x==y)&&(z<y).
 
-    result=(x==y)|| (z<y);
-    printf("(x==y)||(z<y)is %d \n",result);
+    result=(x==y)|| (z<y);  // Assign the result of the logical expression (x==y)||(z<y) to the variable result.
+    printf("(x==y)||(z<y) is %d \n",result);  // Print the value of the logical expression (x==y)||(z<y).
 
-    result=(x!=y)|| (z<y);
-    printf("(x!=y)||(z<y)is %d \n",result);
+    result=(x!=y)|| (z<y);  // Assign the result of the logical expression (x!=y)||(z<y) to the variable result.
+    printf("(x!=y)||(z<y) is %d \n",result);  // Print the value of the logical expression (x!=y)||(z<y).
 
-    result=!(z!=y);
-    printf("!(x!=y) is %d \n",result);
+    result=!(z!=y);  // Assign the result of the logical expression !(z!=y) to the variable result.
+    printf("!(z!=y) is %d \n",result);  // Print the value of the logical expression !(z!=y).
 
-    result=(x==y);
-    printf("(x==y)is %d \n",result);
+    result=(x==y);  // Assign the result of the logical expression (x==y) to the variable result.
+    printf("(x==y) is %d \n",result);  // Print the value of the logical expression (x==y).
 
-    return 0;
+    return 0;  // This line specifies that the main() function should return an integer value of 0 to indicate successful execution of the program.
 }
